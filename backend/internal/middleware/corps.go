ab10f13d2ffef24438655d21bdd5349c7f0181c9
aikazhakh <aidyn.kazhakhmet@nu.edu.kz>
2024-11-24 21:29:08 +0500

    CORS removing credential requiremnet

--- backend/internal/middleware/corps.go
+++ backend/internal/middleware/corps.go
@@ -10,18 +10,17 @@ func CORS(next http.Handler) http.Handler {
 		origin := r.Header.Get("Origin")
 		log.Printf("CORS Middleware: %s %s Origin: %s", r.Method, r.URL.Path, origin)
 
-		// Allow all origins
 		if origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			// w.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
 
 		// Handle preflight (OPTIONS) requests
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
-			log.Println("CORS Middleware: Preflight request handled")
+			// log.Println("CORS Middleware: Preflight request handled")
 			return
 		}
 
