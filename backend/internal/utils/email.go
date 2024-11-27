5f8c199c547f0505a58e5568ead9283a83404059
aikazhakh <aidyn.kazhakhmet@nu.edu.kz>
2024-11-27 13:42:40 +0500

    Email with mailtrap.io as the Domain

--- backend/internal/utils/email.go
+++ backend/internal/utils/email.go
@@ -8,18 +8,18 @@ import (
 
 func SendEmail(to string, subject string, body string) error {
 	mailer := gomail.NewMessage()
-	mailer.SetHeader("From", "no-reply@farmermarketsystem.com")
+	mailer.SetHeader("From", "no-reply@mailtrap.io")
 	mailer.SetHeader("To", to)
 	mailer.SetHeader("Subject", subject)
 	mailer.SetBody("text/plain", body)
 
 	dialer := gomail.NewDialer("live.smtp.mailtrap.io", 587, "api", "ed3092461eda54302535bcee6a6aeed1")
 
-    if err := dialer.DialAndSend(mailer); err != nil {
-        log.Printf("Email send error: %v", err)
-        return err
-    }
+	if err := dialer.DialAndSend(mailer); err != nil {
+		log.Printf("Email send error: %v", err)
+		return err
+	}
 
-    log.Println("Email sent successfully!")
-    return nil
+	log.Println("Email sent successfully!")
+	return nil
 }
